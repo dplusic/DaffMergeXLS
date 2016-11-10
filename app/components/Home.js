d7c3f8767ddf64e606aa68efdd6971854cf781aa
Dongwoon Son <dplusic@gmail.com>
2016-11-11 00:04:50 +0900

    Construct home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,30 @@
 // @flow
+import { remote } from 'electron';
 import React, { Component } from 'react';
+import { Button, ProgressCircle } from 'react-desktop/windows';
 import styles from './Home.css';
 
 
 export default class Home extends Component {
+  static abort() {
+    remote.getCurrentWindow().close();
+  }
+
   render() {
     return (
       <div>
         <div className={styles.container}>
+          <ProgressCircle
+            color="#cccccc"
+            size="70"
+            style={{ margin: '40px auto' }}
+          />
+          <Button
+            push
+            onClick={Home.abort}
+          >
+            Abort
+          </Button>
         </div>
       </div>
     );
