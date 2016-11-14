24772ec413d6a3cc26079447917ca5238578e0c3
Dongwoon Son <dplusic@gmail.com>
2016-11-14 18:52:22 +0900

    Add fs-promise

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -36,6 +36,7 @@ export default validate({
   externals: [
     // put your node 3rd party libraries which can't be built with webpack here
     // (mysql, mongodb, and so on..)
+    'fs-promise',
     'xlsx-plus',
     'daff'
   ]
